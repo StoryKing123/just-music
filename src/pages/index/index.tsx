6754b14781b5ddf8b706bcb6825402ff982ff181
firengxuan <568420804@qq.com>
2022-03-03 23:30:40 +0800

    feat: create

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 const Index: FC = () => {
     return (
-        <div className="font-bold text-base  bg-off-base">
+        <div className="font-bold text-base  bg-base">
             index 21312 32123
         </div>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
     content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
     theme: {
         extend: {},
+        backgroundColor: {
+            base: "var(--color-base)",
+        },
         textColor: {
             base: "var(--color-text-base)",
         },
