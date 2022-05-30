0c980e693d7bae1fdd60ec9a014010d63854b54e
firengxuan <568420804@qq.com>
2022-05-30 08:49:53 +0800

    feat: add net ease source

--- src-tauri/src/command/song.rs
+++ src-tauri/src/command/song.rs
@@ -1,10 +1,12 @@
 use futures::executor::block_on;
 use futures::FutureExt;
+use serde::__private::de::Borrowed;
 use std::borrow::Cow;
 use unm_engine::executor::Executor;
 // use mimalloc::MiMalloc;
 use unm_engine_bilibili::{BilibiliEngine, ENGINE_ID as BILIBILI_ENGINE_ID};
 use unm_engine_migu::ENGINE_ID as MIGU_ENGINE_ID;
+use unm_engine_pyncm::ENGINE_ID as NET_EAST_ENGINE_ID;
 use unm_engine_qq::ENGINE_ID as QQ_ENGINE_ID;
 use unm_types::{Artist, Context, Song};
 
@@ -26,7 +28,10 @@ pub async fn get_song_url(name: String, artist: String) -> Result<String, String
 
     let search_result = executor
         .search(
-            &[std::borrow::Cow::Borrowed(MIGU_ENGINE_ID)],
+            &[
+                std::borrow::Cow::Borrowed(MIGU_ENGINE_ID),
+                std::borrow::Cow::Borrowed(NET_EAST_ENGINE_ID),
+            ],
             &song,
             &context,
         )
