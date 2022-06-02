02543fd19cdc6af58f43201f0cccad93dc330082
firengxuan <568420804@qq.com>
2022-06-02 09:20:49 +0800

    feat: add migu source

--- src-tauri/src/command/song.rs
+++ src-tauri/src/command/song.rs
@@ -14,7 +14,7 @@ use unm_api_utils::executor::build_full_executor;
 
 #[tauri::command]
 pub fn my_custom_command() {
-    println!("I was invoked from JS!");
+    // println!("I was invoked from JS!");
 }
 
 #[tauri::command]
@@ -29,17 +29,34 @@ pub async fn get_song_url(name: String, artist: String) -> Result<String, String
     let search_result = executor
         .search(
             &[
-                // std::borrow::Cow::Borrowed(MIGU_ENGINE_ID),
+                std::borrow::Cow::Borrowed(MIGU_ENGINE_ID),
                 std::borrow::Cow::Borrowed(NET_EAST_ENGINE_ID),
             ],
             &song,
             &context,
         )
-        .await
-        .unwrap();
+        .await;
+    match search_result {
+        Ok(result) => {
+            let result = executor.retrieve(&result, &context).await.unwrap();
+            return Ok(result.url.into());
+        }
+        Err(e) => return Ok(String::from("")),
+    }
+    // let search_result = executor
+    //     .search(
+    //         &[
+    //             // std::borrow::Cow::Borrowed(MIGU_ENGINE_ID),
+    //             std::borrow::Cow::Borrowed(NET_EAST_ENGINE_ID),
+    //         ],
+    //         &song,
+    //         &context,
+    //     )
+    //     .await
+    // .unwrap();
 
-    let result = executor.retrieve(&search_result, &context).await.unwrap();
-    Ok(result.url.into())
+    // let result = executor.retrieve(&search_result, &context).await.unwrap();
+    // Ok(result.url.into())
     // result.url.into()
     // println!("end {}", result.url);
 }
